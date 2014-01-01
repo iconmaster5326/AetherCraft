492d7cd58fb331fdb0e4130a10c9eae0ed75c4ba
Iconmaster <crazybonesj@yahoo.com>
2014-01-01 14:16:59 -0500

    Moved everything to one TE-model

--- com/iconmaster/aec/common/tileentity/TileEntityAetherExtractor.java
+++ com/iconmaster/aec/common/tileentity/TileEntityAetherExtractor.java
@@ -23,69 +23,12 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.PacketDispatcher;
 import cpw.mods.fml.relauncher.Side;
 
-public class TileEntityAetherExtractor extends TileEntity implements
+public class TileEntityAetherExtractor extends AetherCraftTileEntity implements
 		ISidedInventory, IAetherStorage {
-	public static final byte energyBlockType = AetherCraft.GUI_ID_EXTRACTOR;
-
-	private ItemStack[] inventory;
-	private float energy;
-	private int progress;
-	private boolean powered;
-	private float max;
-	private boolean polled = false;
-
-	private boolean db = false;
-
 	public TileEntityAetherExtractor() {
+		energyBlockType = AetherCraft.GUI_ID_EXTRACTOR;
 		max = Float.parseFloat(AetherCraft.getOptions("ammaxstorage"))/2;
 		inventory = new ItemStack[8];
-	}
-
-	public int getSizeInventory() {
-		return this.inventory.length;
-	}
-
-	public ItemStack getStackInSlot(int slot) {
-		return inventory[slot];
-	}
-
-	@Override
-	public ItemStack decrStackSize(int slot, int amount) {
-		ItemStack stack = this.getStackInSlot(slot);
-
-		if (stack != null) {
-			if (stack.stackSize <= amount) {
-				this.setInventorySlotContents(slot, null);
-			} else {
-				stack = stack.splitStack(amount);
-
-				if (stack.stackSize == 0) {
-					this.setInventorySlotContents(slot, null);
-				}
-			}
-		}
-
-		return stack;
-	}
-
-	@Override
-	public ItemStack getStackInSlotOnClosing(int slot) {
-		ItemStack stack = this.getStackInSlot(slot);
-
-		if (stack != null) {
-			setInventorySlotContents(slot, null);
-		}
-
-		return stack;
-	}
-
-	@Override
-	public void setInventorySlotContents(int slot, ItemStack stack) {
-		this.inventory[slot] = stack;
-
-		if (stack != null && stack.stackSize > this.getInventoryStackLimit()) {
-			stack.stackSize = this.getInventoryStackLimit();
-		}
 	}
 
 	@Override
@@ -94,77 +37,7 @@ public class TileEntityAetherExtractor extends TileEntity implements
 	}
 
 	@Override
-	public boolean isInvNameLocalized() {
-		return true;
-	}
-
-	@Override
-	public int getInventoryStackLimit() {
-		return 64;
-	}
-
-	@Override
-	public boolean isUseableByPlayer(EntityPlayer player) {
-		return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord,
-				this.zCoord) == this
-				&& player.getDistanceSq(this.xCoord + 0.5, this.yCoord + 0.5,
-						this.zCoord + 0.5) < 64;
-	}
-
-	@Override
-	public void openChest() {
-	}
-
-	@Override
-	public void closeChest() {
-	}
-
-	@Override
-	public boolean isItemValidForSlot(int slot, ItemStack stack) {
-		return true;
-	}
-
-	@Override
-	public void readFromNBT(NBTTagCompound tagCompound) {
-		super.readFromNBT(tagCompound);
-		NBTTagList tagList = tagCompound.getTagList("Inventory");
-
-		for (int i = 0; i < tagList.tagCount(); i++) {
-			NBTTagCompound tag = (NBTTagCompound) tagList.tagAt(i);
-			byte slot = tag.getByte("Slot");
-
-			if (slot >= 0 && slot < inventory.length) {
-				inventory[slot] = ItemStack.loadItemStackFromNBT(tag);
-			}
-		}
-
-		this.energy = tagCompound.getFloat("AV");
-	}
-
-	@Override
-	public void writeToNBT(NBTTagCompound tagCompound) {
-		super.writeToNBT(tagCompound);
-		NBTTagList itemList = new NBTTagList();
-
-		for (int i = 0; i < inventory.length; i++) {
-			ItemStack stack = inventory[i];
-
-			if (stack != null) {
-				NBTTagCompound tag = new NBTTagCompound();
-				tag.setByte("Slot", (byte) i);
-				stack.writeToNBT(tag);
-				itemList.appendTag(tag);
-			}
-		}
-
-		tagCompound.setTag("Inventory", itemList);
-		tagCompound.setFloat("AV", this.energy);
-	}
-
-	private void handleAether() {
-		if (db ) {return;}
-		db = true;
-
+	public void handleAether() {
 		ItemStack topStack = this.getStackInSlot(0);
 		ItemStack currentStack;
 		boolean doneSomething = false;
@@ -225,153 +98,13 @@ public class TileEntityAetherExtractor extends TileEntity implements
 			if (!Boolean.parseBoolean(AetherCraft
 					.getOptions("instantconsume")) && doneSomething) {
 				this.sync();
-				db = false;
 				return;
 			}
 		}
 		if (doneSomething) {
 			this.sync();
-			db = false;
 			return;
 		}
-		db = false;
-	}
-
-	public void setPoweredState(boolean state) {
-		this.powered = state;
-	}
-
-	@Override
-	public void updateEntity() {
-		this.calculateProgress();
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER && !this.powered) {
-			handleAether();
-		}
-	}
-
-	public void sync() {
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			DataOutputStream outputStream = new DataOutputStream(bos);
-
-			try {
-				outputStream.writeByte(this.energyBlockType);
-				outputStream.writeInt(this.xCoord);
-				outputStream.writeInt(this.yCoord);
-				outputStream.writeInt(this.zCoord);
-				outputStream.writeFloat(this.energy);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-
-			Packet250CustomPayload packet = new Packet250CustomPayload();
-			packet.channel = "Aec";
-			packet.data = bos.toByteArray();
-			packet.length = bos.size();
-			if (this.worldObj != null && this.worldObj.provider != null) {
-				PacketDispatcher.sendPacketToAllPlayers(packet);
-			}
-		}
-	}
-
-	public void recieveSync(float par1energy) {
-		this.energy = par1energy;
-	}
-
-	public int getStackableSlot(ItemStack cStack) {
-		ItemStack stack;
-		if (cStack == null) {return 1;}
-		for (int i = 1; i < this.inventory.length; i++) {
-			stack = this.getStackInSlot(i);
-
-			if (stack != null && stack.isItemEqual(cStack)
-					&& stack.stackSize < this.getInventoryStackLimit()
-					&& stack.stackSize < stack.getMaxStackSize()) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public int getEmptySlot() {
-		ItemStack stack;
-		for (int i = 1; i < this.inventory.length; i++) {
-			stack = this.getStackInSlot(i);
-			if (stack == null) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	@Override
-	public float addAether(float ev) {
-		if (this.energy + ev <= max) {
-			this.energy += ev;
-			this.sync();
-			return 0;
-		} else {
-			float rest = (this.energy + ev) - max;
-			this.energy = max;
-			this.sync();
-			return rest;
-		}
-	}
-
-	@Override
-	public float extractAether(float av) {
-		//System.out.println("Got "+av+" AV.");
-		if (this.energy - av >= 0) {
-			//System.out.println("Had enough AV. Returning "+av+" AV.");
-			this.energy -= av;
-			this.sync();
-			return av;
-		}
-		float rest = this.energy;
-		//System.out.println("Did not have enough AV. Returning "+rest+" AV.");
-		this.energy = 0;
-		this.sync();
-		return rest;
-	}
-
-	@Override
-	public float getAether() {
-		//System.out.println("Returning "+this.energy);
-		if (!polled) {
-			requestSync();
-			polled = true;
-		}
-		return this.energy;
-	}
-	
-	@Override
-	public float tryAddAether(float ev) {
-		if (this.energy + ev <= max) {
-			//this.energy += ev;
-			//this.sync();
-			return 0;
-		} else {
-			float rest = (this.energy + ev) - max;
-			//this.energy = max;
-			//this.sync();
-			return rest;
-		}
-	}
-
-	@Override
-	public float tryExtractAether(float av) {
-		//System.out.println("Got "+av+" AV.");
-		if (this.energy - av >= 0) {
-			//System.out.println("Had enough AV. Returning "+av+" AV.");
-			//this.energy -= av;
-			//this.sync();
-			return av;
-		}
-		float rest = this.energy;
-		//System.out.println("Did not have enough AV. Returning "+rest+" AV.");
-		//this.energy = 0;
-		//this.sync();
-		return rest;
 	}
 
 	@Override
@@ -396,50 +129,11 @@ public class TileEntityAetherExtractor extends TileEntity implements
 		return true;
 	}
 	
-	public float getPossibleAether() {
-		return getAether() + AetherNetwork.getStoredAV(worldObj, xCoord, yCoord, zCoord);
-	}
-	
-	public boolean canConsume(ItemStack currentStack) {
-		if (currentStack == null) {
-			return false;
-		}
-		float av = AVRegistry.getAV(currentStack);
-		if (av<=0 ) {
-			return false;
-		}
-
-		return true;
-	}
-
+	@Override
 	public void calculateProgress() {
 		this.progress = (int) ((this.energy/max)*100);
 		if (this.progress > 100) {
 			this.progress = 100;
 		}
-	}
-
-	public int getProgress() {
-		return this.progress;
-	}
-	
-	public void requestSync() {
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		DataOutputStream outputStream = new DataOutputStream(bos);
-
-		try {
-			outputStream.writeByte(this.energyBlockType);
-			outputStream.writeInt(this.xCoord);
-			outputStream.writeInt(this.yCoord);
-			outputStream.writeInt(this.zCoord);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		Packet250CustomPayload packet = new Packet250CustomPayload();
-		packet.channel = "AecReq";
-		packet.data = bos.toByteArray();
-		packet.length = bos.size();
-		PacketDispatcher.sendPacketToServer(packet);
 	}
 }
--- com/iconmaster/aec/common/tileentity/TileEntityAetherManipulator.java
+++ com/iconmaster/aec/common/tileentity/TileEntityAetherManipulator.java
@@ -23,72 +23,11 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.PacketDispatcher;
 import cpw.mods.fml.relauncher.Side;
 
-public class TileEntityAetherManipulator extends TileEntity implements
+public class TileEntityAetherManipulator extends AetherCraftTileEntity implements
 		ISidedInventory, IAetherStorage {
-	public static final byte energyBlockType = AetherCraft.GUI_ID_MANIPULATOR;
-
-	private ItemStack[] inventory;
-	private float energy;
-	private int progress;
-	private boolean powered;
-
-	// 0 = Bottom, 1 = Top, 2 = North, 3 = South, 4 = East, 5 = West
-	private boolean connectedSides[];
-
-	private boolean db = false;
-
-	private boolean polled = false;
-
 	public TileEntityAetherManipulator() {
+		energyBlockType = AetherCraft.GUI_ID_MANIPULATOR;
 		inventory = new ItemStack[55];
-		connectedSides = new boolean[6];
-	}
-
-	public int getSizeInventory() {
-		return this.inventory.length;
-	}
-
-	public ItemStack getStackInSlot(int slot) {
-		return inventory[slot];
-	}
-
-	@Override
-	public ItemStack decrStackSize(int slot, int amount) {
-		ItemStack stack = this.getStackInSlot(slot);
-
-		if (stack != null) {
-			if (stack.stackSize <= amount) {
-				this.setInventorySlotContents(slot, null);
-			} else {
-				stack = stack.splitStack(amount);
-
-				if (stack.stackSize == 0) {
-					this.setInventorySlotContents(slot, null);
-				}
-			}
-		}
-
-		return stack;
-	}
-
-	@Override
-	public ItemStack getStackInSlotOnClosing(int slot) {
-		ItemStack stack = this.getStackInSlot(slot);
-
-		if (stack != null) {
-			setInventorySlotContents(slot, null);
-		}
-
-		return stack;
-	}
-
-	@Override
-	public void setInventorySlotContents(int slot, ItemStack stack) {
-		this.inventory[slot] = stack;
-
-		if (stack != null && stack.stackSize > this.getInventoryStackLimit()) {
-			stack.stackSize = this.getInventoryStackLimit();
-		}
 	}
 
 	@Override
@@ -96,81 +35,7 @@ public class TileEntityAetherManipulator extends TileEntity implements
 		return "gra.tileentityem";
 	}
 
-	@Override
-	public boolean isInvNameLocalized() {
-		return true;
-	}
-
-	@Override
-	public int getInventoryStackLimit() {
-		return 64;
-	}
-
-	@Override
-	public boolean isUseableByPlayer(EntityPlayer player) {
-		return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord,
-				this.zCoord) == this
-				&& player.getDistanceSq(this.xCoord + 0.5, this.yCoord + 0.5,
-						this.zCoord + 0.5) < 64;
-	}
-
-	@Override
-	public void openChest() {
-	}
-
-	@Override
-	public void closeChest() {
-	}
-
-	@Override
-	public boolean isItemValidForSlot(int slot, ItemStack stack) {
-		if (slot > 0) {
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public void readFromNBT(NBTTagCompound tagCompound) {
-		super.readFromNBT(tagCompound);
-		NBTTagList tagList = tagCompound.getTagList("Inventory");
-
-		for (int i = 0; i < tagList.tagCount(); i++) {
-			NBTTagCompound tag = (NBTTagCompound) tagList.tagAt(i);
-			byte slot = tag.getByte("Slot");
-
-			if (slot >= 0 && slot < inventory.length) {
-				inventory[slot] = ItemStack.loadItemStackFromNBT(tag);
-			}
-		}
-
-		this.energy = tagCompound.getFloat("AV");
-	}
-
-	@Override
-	public void writeToNBT(NBTTagCompound tagCompound) {
-		super.writeToNBT(tagCompound);
-		NBTTagList itemList = new NBTTagList();
-
-		for (int i = 0; i < inventory.length; i++) {
-			ItemStack stack = inventory[i];
-
-			if (stack != null) {
-				NBTTagCompound tag = new NBTTagCompound();
-				tag.setByte("Slot", (byte) i);
-				stack.writeToNBT(tag);
-				itemList.appendTag(tag);
-			}
-		}
-
-		tagCompound.setTag("Inventory", itemList);
-		tagCompound.setFloat("AV", this.energy);
-	}
-
-	private void handleAether() {
-		if (db ) {return;}
-		db = true;
+	public void handleAether() {
 		float emMaxStorage = Float.parseFloat(AetherCraft.getOptions("ammaxstorage"));
 
 		ItemStack topStack = this.getStackInSlot(0);
@@ -282,59 +147,13 @@ public class TileEntityAetherManipulator extends TileEntity implements
 			if (!Boolean.parseBoolean(AetherCraft
 					.getOptions("instantconsume")) && i >= 9 && doneSomething) {
 				this.sync();
-				db = false;
 				return;
 			}
 		}
 		if (doneSomething) {
 			this.sync();
-			db = false;
 			return;
 		}
-		db = false;
-	}
-
-	public void setPoweredState(boolean state) {
-		this.powered = state;
-	}
-
-	@Override
-	public void updateEntity() {
-		this.calculateProgress();
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER
-				&& !this.powered) {
-//			this.updateConnectedSides();
-			handleAether();
-		}
-	}
-
-	public void sync() {
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			DataOutputStream outputStream = new DataOutputStream(bos);
-
-			try {
-				outputStream.writeByte(this.energyBlockType);
-				outputStream.writeInt(this.xCoord);
-				outputStream.writeInt(this.yCoord);
-				outputStream.writeInt(this.zCoord);
-				outputStream.writeFloat(this.energy);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-
-			Packet250CustomPayload packet = new Packet250CustomPayload();
-			packet.channel = "Aec";
-			packet.data = bos.toByteArray();
-			packet.length = bos.size();
-			if (this.worldObj != null && this.worldObj.provider != null) {
-				PacketDispatcher.sendPacketToAllPlayers(packet);
-			}
-		}
-	}
-
-	public void recieveSync(float par1energy) {
-		this.energy = par1energy;
 	}
 
 	public void calculateProgress() {
@@ -350,100 +169,6 @@ public class TileEntityAetherManipulator extends TileEntity implements
 		} else {
 			this.progress = 0;
 		}
-	}
-
-	public int getProgress() {
-		return this.progress;
-	}
-
-	public int getStackableSlot(ItemStack cStack) {
-		ItemStack stack;
-		if (cStack == null) {return 1;}
-		for (int i = 1; i < this.inventory.length; i++) {
-			stack = this.getStackInSlot(i);
-
-			if (stack != null && stack.isItemEqual(cStack)
-					&& stack.stackSize < this.getInventoryStackLimit()
-					&& stack.stackSize < stack.getMaxStackSize()) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public int getEmptySlot() {
-		ItemStack stack;
-		for (int i = 1; i < this.inventory.length; i++) {
-			stack = this.getStackInSlot(i);
-			if (stack == null) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	@Override
-	public float addAether(float ev) {
-		float ammaxstorage = Float.parseFloat(AetherCraft
-				.getOptions("ammaxstorage"));
-		if (this.energy + ev <= ammaxstorage) {
-			this.energy += ev;
-			this.sync();
-			return 0;
-		} else {
-			float rest = (this.energy + ev) - ammaxstorage;
-			this.energy = ammaxstorage;
-			this.sync();
-			return rest;
-		}
-	}
-
-	@Override
-	public float extractAether(float av) {
-		//System.out.println("Got "+av+" AV.");
-		if (this.energy - av >= 0) {
-			//System.out.println("Had enough AV. Returning "+av+" AV.");
-			this.energy -= av;
-			this.sync();
-			return av;
-		}
-		float rest = this.energy;
-		//System.out.println("Did not have enough AV. Returning "+rest+" AV.");
-		this.energy = 0;
-		this.sync();
-		return rest;
-	}
-	
-	@Override
-	public float tryAddAether(float ev) {
-		float ammaxstorage = Float.parseFloat(AetherCraft
-				.getOptions("ammaxstorage"));
-		if (this.energy + ev <= ammaxstorage) {
-			//this.energy += ev;
-			//this.sync();
-			return 0;
-		} else {
-			float rest = (this.energy + ev) - ammaxstorage;
-			//this.energy = ammaxstorage;
-			//this.sync();
-			return rest;
-		}
-	}
-
-	@Override
-	public float tryExtractAether(float av) {
-		//System.out.println("Got "+av+" AV.");
-		if (this.energy - av >= 0) {
-			//System.out.println("Had enough AV. Returning "+av+" AV.");
-			//this.energy -= av;
-			//this.sync();
-			return av;
-		}
-		float rest = this.energy;
-		//System.out.println("Did not have enough AV. Returning "+rest+" AV.");
-		//this.energy = 0;
-		//this.sync();
-		return rest;
 	}
 
 	@Override
@@ -496,62 +221,5 @@ public class TileEntityAetherManipulator extends TileEntity implements
 			}
 		}
 		return false;
-	}
-	
-	public float getPossibleAether() {
-		return getAether() + AetherNetwork.getStoredAV(worldObj, xCoord, yCoord, zCoord);
-	}
-	
-	public boolean canConsume(ItemStack currentStack) {
-		ItemStack topStack = this.getStackInSlot(0);
-		if (currentStack == null) {
-			return false;
-		}
-		if (topStack != null && currentStack.itemID == topStack.itemID && currentStack.getItemDamage() == topStack.getItemDamage()) {
-			return false;
-		}
-		float av = AVRegistry.getAV(currentStack);
-		if (av<=0 ) {
-			return false;
-		}
-//		if (getAether()+av>Float.parseFloat(AetherCraft.getOptions("ammaxstorage"))) {
-//			return false;
-//		}
-		return true;
-	}
-	public boolean canProduce(ItemStack currentItem) {
-		float av = AVRegistry.getAV(currentItem);
-		if (this.getStackableSlot(currentItem)<=0 && this.getEmptySlot() <=0) {return false;}
-		return av>0 && av<=getPossibleAether();
-	}
-
-	@Override
-	public float getAether() {
-		//System.out.println("Returning "+this.energy);
-		if (!polled) {
-			requestSync();
-			polled = true;
-		}
-		return this.energy;
-	}
-
-	public void requestSync() {
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		DataOutputStream outputStream = new DataOutputStream(bos);
-	
-		try {
-			outputStream.writeByte(this.energyBlockType);
-			outputStream.writeInt(this.xCoord);
-			outputStream.writeInt(this.yCoord);
-			outputStream.writeInt(this.zCoord);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	
-		Packet250CustomPayload packet = new Packet250CustomPayload();
-		packet.channel = "AecReq";
-		packet.data = bos.toByteArray();
-		packet.length = bos.size();
-		PacketDispatcher.sendPacketToServer(packet);
 	}
 }
