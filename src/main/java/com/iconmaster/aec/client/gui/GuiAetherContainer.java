d175f5c5d527e478aa3ec19a1e98e659fab83b96
Iconmaster <crazybonesj@yahoo.com>
2014-05-09 16:14:04 -0400

    Fixed Container GUI

--- src/main/java/com/iconmaster/aec/client/gui/GuiAetherContainer.java
+++ src/main/java/com/iconmaster/aec/client/gui/GuiAetherContainer.java
@@ -38,9 +38,6 @@ public class GuiAetherContainer extends AetherCraftGui<TileEntityAetherContainer
 		this.fontRendererObj.drawStringWithShadow(
 				"AV: " + NumberUtils.display(te.getAether())+"/"+NumberUtils.display(te.max), 34, 38,
 				0x55FF55);
-		
-		te.calcLimit();
-		this.fontRendererObj.drawStringWithShadow("Limit: "+NumberUtils.display(te.limit), 34, 50,0x00FF00);
 
 		int a = 68 - 16;
 		float b = (100 - this.te.getProgress()) / 100.0f;
