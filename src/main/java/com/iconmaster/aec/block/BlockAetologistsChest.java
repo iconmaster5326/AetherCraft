20d85b25a5f10d938384e87ebd9115b0656b4374
Iconmaster <crazybonesj@yahoo.com>
2014-08-04 12:20:32 -0400

    Added chest textures

--- src/main/java/com/iconmaster/aec/block/BlockAetologistsChest.java
+++ src/main/java/com/iconmaster/aec/block/BlockAetologistsChest.java
@@ -1,11 +1,15 @@
 package com.iconmaster.aec.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 import com.iconmaster.aec.AetherCraft;
@@ -16,6 +20,11 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockAetologistsChest extends BlockContainer implements IAetherTransfer {
+	public IIcon topIcon;
+	public IIcon sideIcon;
+	public IIcon bottomIcon;
+	public IIcon frontIcon;
+	
 	public BlockAetologistsChest(Material material) {
 		super(material);
 		this.setCreativeTab(AetherCraft.tabAetherCraft);
@@ -50,16 +59,60 @@ public class BlockAetologistsChest extends BlockContainer implements IAetherTran
 		return (float) ((Float.parseFloat(AetherCraft.getOptions("flowrate")))*(Math.pow(2,2*2)));
 	}
 	
-	    @Override
-	    @SideOnly(Side.CLIENT)
-	    public void registerBlockIcons(IIconRegister iconRegister)
-	    {
-	        blockIcon = iconRegister.registerIcon("aec:aetoChest");
-	    }
-	    
-	    @Override
-	    @SideOnly(Side.CLIENT)
-	    public IIcon getIcon(int side,int meta) {
-	    	return blockIcon;
-	    }
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+       frontIcon = iconRegister.registerIcon("aec:aetoChestFront");
+       sideIcon = iconRegister.registerIcon("aec:aetoChestSide");
+       topIcon = iconRegister.registerIcon("aec:aetoChestTop");
+       bottomIcon = iconRegister.registerIcon("aec:aetoChestBottom");
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side,int meta) {
+    	if (meta==0 && side==3) {
+    		return frontIcon;
+    	}
+    	
+    	if (side==1) {
+    		return topIcon;
+    	}
+    	
+    	if (side==0) {
+    		return bottomIcon;
+    	}
+    	
+    	return side==meta? frontIcon : sideIcon;
+    }
+    
+    @Override
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack)
+    {
+        byte b0 = 0;
+        int l = MathHelper.floor_double((double)(player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            b0 = 2;
+        }
+
+        if (l == 1)
+        {
+            b0 = 5;
+        }
+
+        if (l == 2)
+        {
+            b0 = 3;
+        }
+
+        if (l == 3)
+        {
+            b0 = 4;
+        }
+
+        world.setBlockMetadataWithNotify(x, y, z, b0, 3);
+    }
 }
