00687db2dd8338f56cfb29d3b98133a622f0a12a
AmethystAir <amethystair@gmail.com>
2014-10-27 02:02:26 -0600

    I derped on the last commit
    I accidentally the exclamation point, and the player would take fall damage even if they had the ring enabled. Removing the exclamation point fixes that, and still only calls the function once.

--- src/main/java/com/iconmaster/aec/item/ItemPhasingRing.java
+++ src/main/java/com/iconmaster/aec/item/ItemPhasingRing.java
@@ -58,7 +58,7 @@ public class ItemPhasingRing extends Item implements IAetherRing {
 	
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
-		if (!world.isRemote) {
+		if (world.isRemote) {
 			return stack;
 		}
 		
